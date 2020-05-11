bb0f459660467988ae9c00cb111811adc587ca55
KuznetsovMA <jztenk@gmail.com>
2020-05-11 16:59:47 +0300

    Float aside & 1280 width to 1285

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './assets/scss/index.scss'
+
+const roomTable = document.querySelector('.room-table')
+
+document.addEventListener('scroll', () => {
+  roomTable.classList.toggle('room-table--float', document.documentElement.scrollLeft + 1 >= roomTable.getBoundingClientRect().width)
+})
