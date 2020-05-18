1ea90eba5bec3e2dfed315384ba9cb5d19f3151c
KuznetsovMA <jztenk@gmail.com>
2020-05-18 21:34:22 +0300

    Slot template refactoring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import './assets/scss/index.scss'
 
 const roomTable = document.querySelector('.room-table')
+const slots = document.querySelectorAll('.slot--reserved .slot__button')
 
 document.querySelector('.site').addEventListener('scroll', () => {
   roomTable.classList.toggle('room-table--float', document.querySelector('.site').scrollLeft + 1 >= roomTable.getBoundingClientRect().width)
 })
+
+for (const slot of slots) {
+  const widget = slot.nextElementSibling
+  const editButton = widget.querySelector('.slot-widget__edit')
+
+  slot.addEventListener('focus', () => widget.classList.remove('hidden'))
+  editButton.addEventListener('blur', () => widget.classList.add('hidden'))
+}
